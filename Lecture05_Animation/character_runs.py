3038e62389d4b729f60dbc0e918f71b5a85016d8
EUM-Desktop <undermilk12@gmail.com>
2024-09-30 21:34:40 +0900

    move_my_character_with_key.py 생성

--- Lecture05_Animation/character_runs.py
+++ Lecture05_Animation/character_runs.py
@@ -5,8 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-# fill here
+frame = 0
+for x in range(0, 800, 10):
+    clear_canvas()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
 
+delay(5)
 
 close_canvas()
 
